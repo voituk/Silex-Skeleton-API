e8a80c2e4573d5d03ed0354948bce9427674afb9
Vadim Voituk <vadim.voituk@gmail.com>
2013-05-16 19:43:27 +0200

    Removed some non-API oriented packages

--- src/controllers.php
+++ src/controllers.php
@@ -7,17 +7,32 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+		return $app->json(array(
+			'code' => 0, 
+			'message' => 'OK',
+		));
 })
 ->bind('homepage')
 ;
 
+/**
+ * Configure error handler
+ */
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
-        return;
+        return null;
+    }
+    $message = 'We are sorry, but something went terribly wrong.';
+
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
     }
 
-    $page = 404 == $code ? '404.html' : '500.html';
-
-    return new Response($app['twig']->render($page, array('code' => $code)), $code);
+    return $app->json(array(
+        'code'    => $code,
+        'message' => $message
+    ), $code);
 });
+
